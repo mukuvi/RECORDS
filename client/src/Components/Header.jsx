29c54e20b9a2371a58cb09a568291f9cb45ed18b
jmngandu <jamesmngandu@gmail.com>
2025-02-14 10:10:17 +0300

    records

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <div className="header-logo">JTG</div>
+        <div className="navs-btn"></div>
+      </div>
+    </>
+  );
+}
